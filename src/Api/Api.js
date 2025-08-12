8cd4b02d479e35b21cecfd6c267cd51f484d4771
Stepan Trubcov <stepantrubcov@MacBook-Air-Stepan.local>
2025-08-12 08:47:45 +0300

    new commit

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -4,21 +4,22 @@ export const addProfile = async (setUser) => {
         WebApp.ready();
 
         const userData = WebApp.initDataUnsafe?.user;
-      
+      console.log(WebApp)
+      console.log(WebApp.initDataUnsafe)
         if (!userData) {
           console.error("Нет данных пользователя от Telegram");
           return;
         }
       
         // Сохраняем пользователя в БД
-        await fetch("http://localhost:5000/api/profile", {
+        await fetch("https://f7a2d8a411c3.ngrok-free.app/api/profile", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(userData)
         });
       
         // Получаем данные из БД
-        const res = await fetch(`http://localhost:5000/api/profile/${userData.id}`);
+        const res = await fetch(`https://f7a2d8a411c3.ngrok-free.app/api/profile/${userData.id}`);
         const data = await res.json();
       
         setUser(data);
