a33f241a2487eafbae64c34d103b0a958a408820
Stepan Trubcov <stepantrubcov@192.168.0.11>
2025-12-07 16:28:56 +0300

    new update the logic of the calendar, the logic of completing goals, etc., and the addition of a weekly report have been changed

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -14,7 +14,7 @@ const Profile = (props) => {
         <TodaysGoalsConteiner />
         <GenerationButtonConteiner />
         <ContributionCalendarConteiner />
-        <YesterdayConteiner />
+        {/* <YesterdayConteiner /> */}
     </div>
 }
 
