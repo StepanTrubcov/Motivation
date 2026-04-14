b05545378f5ded969237bfa195576b3038891a4a
Stepan Trubcov <stepantrubcov@MacBook-Air-Stepan.local>
2026-04-14 18:25:52 +0300

    Добавил блок новое

--- client/src/components/Profile/ContributionCalendar/ContributionCalendar.jsx
+++ client/src/components/Profile/ContributionCalendar/ContributionCalendar.jsx
@@ -412,6 +412,9 @@ const ContributionCalendar = ({ calendarData }) => {
 
   return (
     <div className={c.calendar} data-tutorial-id="calendar">
+      <div className={c.cornerBadge} aria-hidden>
+        {language === 'ru' ? 'НОВОЕ' : 'NEW'}
+      </div>
       <div ref={scrollerRef} className={c.variantsScroller}>
         <div className={c.variantPage}>{calendarNode}</div>
         <div className={c.variantPage}>
